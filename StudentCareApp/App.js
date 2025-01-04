737445ece6897f70921244cc6f06b00076fc82c0
DilmyPerera <samadiniperera20@gmail.com>
2025-01-04 11:48:14 +0530

    Set up navigation in the app with a Login screen using React Navigation and Safe Area Provider

--- StudentCareApp/App.js
+++ StudentCareApp/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/native-stack';
+import LoginPage from './components/LoginPage'
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+      <Stack.Navigator intialRouteName='Login'
+      screenOptions={{
+        headerStyle :{ backgroundColor : '#e2bee2' },
+      }}
+      >
+      <Stack.Screen  name="Login" component = {LoginPage} options={{title: "Login"}}/>
+      </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
