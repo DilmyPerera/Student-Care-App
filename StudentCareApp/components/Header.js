d600478cfe03480d6fa570a81afe50d057338ff2
DilmyPerera <samadiniperera20@gmail.com>
2025-01-05 09:44:42 +0530

    Update Header,js

--- StudentCareApp/components/Header.js
+++ StudentCareApp/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, ScrollView } from 'react-native';
+import { PaperProvider , Text} from 'react-native-paper';
 
 export default function Header() {
   return (
-    <View style={styles.header}>
-      <Image source={require('../assets/uovlogo.png')} style={styles.logo} />
+    <PaperProvider>
+      <ScrollView>
+        <View style={styles.header}>
+        <Text style={styles.headertext}>UOV Student Care</Text> 
+      {/* <Image source={require('../assets/uovlogo.png')} style={styles.logo} /> */}
     </View>
+      </ScrollView>
+    
+    </PaperProvider>
+    
   );
 }
 
@@ -13,6 +21,12 @@ const styles = StyleSheet.create({
   header: {
     alignItems: 'center',
     marginBottom: 16,
+  },
+  headertext : {
+    backgroundColor:"#8b008b",
+        alignItems:"center", 
+        width:"100%",
+        height:50,
   },
   logo: {
     width: 200,
